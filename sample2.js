cfe73e70642af479b13cf102b5ce5e5e1a2243e5
kavitha <kavitha.subramanian2@cognizant.com>
2014-07-08 12:40:51 +0530

    HandsOn

--- sample2.js
+++ sample2.js
@@ -8,25 +8,16 @@ app.set('views','views');
 app.set('view engine','jade');
 app.use(bodyParser());
 app.use('/', routes);
-//app.use('/users', users);
-//app.use('/user', routes);
+
 app.listen(8000);
-/*app.get('/home',function(req,res)
-{
-    res.render('SampleView',
-        {
-            title : 'Home'
-        });
-    res.send();
-});*/
 
 
 
 
-//app.use(function(req, res, next) {
-//    var err = new Error('Not Found');
-//    err.status = 404;
-//    next(err);
-//});
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 module.exports = app;
