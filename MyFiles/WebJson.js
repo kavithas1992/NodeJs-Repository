2d34fa332d39fe39de0b9539a4ba17f6f722a133
kavithas1992 <kavitha.subramanian2@cognizant.com>
2014-07-09 18:53:40 +0530

    HandsON

--- MyFiles/WebJson.js
+++ MyFiles/WebJson.js
@@ -4,22 +4,6 @@ var fs = require('fs');
 app.get('/data',function(req,resp)
 {
 
-    /*fs.readFile('./WebDataJson.json',function(err,data)
-
-    {
-        if(err)
-        {
-            err;
-        }
-        else
-        {
-            resp.writeHead(200, {"Content-Type":"text/html"});
-            var data = data;
-            data = JSON.parse(data);
-            resp.send(data);
-        }
-    });
-    resp.end();*/
     var data = fs.readFile('./WebDataJson.json', function (err, data) {
         resp.setHeader('Content-Type', 'application/json');
         resp.send(data);
